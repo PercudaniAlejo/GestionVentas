cf2b737a5ba0ad0a67dcf754b0649c42fcd5521b
Alejo Percudani <alejopercudani@outlook.com>
2021-09-19 10:50:38 -0300

    Agregar archivos de proyecto.

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CapaDatos
+{
+    public static class Conexion
+    {
+        //private static string server = @"PC94\SQLEXPRESS";
+        private static string server = @"DESKTOP-RR1BBOV\SQLEXPRESS";
+        private static string db = @"GestionVentas";
+        private static string usuario = @"AlejoDB";
+        private static string clave = @"alejo3898";
+
+        public static string DarStrConexion()
+        {
+            return string.Concat(@"Data Source=", server, ";Initial Catalog=", db,
+                ";Persist Security Info=False;User ID=", usuario, ";Password=", clave);
+        }
+
+    }
+}
